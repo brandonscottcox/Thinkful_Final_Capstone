818294d1f6286b65e85df40243d8b46978595e90
Brandon Cox <brandonscottcox@gmail.com>
2021-10-04 15:22:14 -0600

    all backend tests passing except for 1

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -9,15 +9,20 @@
  const methodNotAllowed = require("../errors/methodNotAllowed");
  
  router
+ .route("/:reservationId/status")
+ .put(controller.updateStatus)
+ .all(methodNotAllowed);
+
+router
+ .route("/:reservationId")
+ .get(controller.read)
+ .put(controller.updateReservation)
+ .all(methodNotAllowed);
+
+router
  .route("/")
  .get(controller.list)
  .post(controller.create)
  .all(methodNotAllowed);
-
- router
-    .route("/:reservationId")
-    .get(controller.read)
-    .put(controller.updateReservation)
-    .all(methodNotAllowed);
  
  module.exports = router;
