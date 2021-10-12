971c0dd752b8d1279eb41fec53209f8a43fcc6cf
Brandon Cox <brandonscottcox@gmail.com>
2021-10-11 21:34:37 -0600

    07 database fix

--- front-end/src/tables/TableCard.jsx
+++ front-end/src/tables/TableCard.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { deletePartyFromTable } from "../utils/api";
+import { useHistory} from "react-router-dom";
 
-export default function TableCard({ calledAPI, setCalledAPI, table }) {
+
+export default function TableCard({ setCalledAPI, calledAPI, table }) {
   const [error, setError] = useState(null);
 
+  const history = useHistory()
+/*
   function handleFinish() {
     const abortController = new AbortController();
     const answer = window.confirm(
@@ -12,6 +16,20 @@ export default function TableCard({ calledAPI, setCalledAPI, table }) {
     if (answer) {
       deletePartyFromTable(table.table_id, abortController.signal)
         .then(() => setCalledAPI(() => !calledAPI))
+        .catch(setError);
+    }
+  }
+  */
+  
+
+  function handleFinish() {
+    const abortController = new AbortController();
+    const answer = window.confirm(
+      "Is this table ready to seat new guests? \n\nThis cannot be undone."
+    );
+    if (answer) {
+      deletePartyFromTable(table.table_id, abortController.signal)
+        .then(history.go(0))
         .catch(setError);
     }
   }
@@ -28,7 +46,7 @@ export default function TableCard({ calledAPI, setCalledAPI, table }) {
           <button
             type="button"
             className="btn btn-danger"
-            onClick={handleFinish}
+            onClick={() => handleFinish()}
             data-table-id-finish={table.table_id}
           >
             Finish
